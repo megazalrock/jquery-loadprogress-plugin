3a9d76d78dcf4e9c89730377adeabd3110df3c7e
otto <otto@mgzl.jp>
2013-01-19 16:35:17 +0900

    デフォルトのCSSを改良

--- grunt.js
+++ grunt.js
@@ -4,67 +4,49 @@ module.exports = function(grunt){
 		lint: {
 			files : [
 				'grunt.js',
-				'src/jquery-1.8.2.js',
-				'src/jquery-ui-1.9.1.custom.js',
-				'src/jquery-parallax-plugin2.js'
+				'src/jquery.loadprogress.js'
 			]
 		},
 		watch : {
-		    dist : {
-		    	files : [
-		    	    'src/jquery-ui-1.9.1.custom.js',
-					'src/jquery-parallax-plugin2.js'
-		    	],
-		    	tasks:'concat min'
-		    },
-		    css : {
-		    	files : [
-					'src/jquery-ui-1.9.1.custom.css',
-					'src/jquery-parallax-plugin2.css'
-		    	],
-		    	tasks:'concat'
-		    },
-		    less : {
-		    	files : [
-		    	       'demo/style.less'
-		    	],
-		    	tasks:'less'
-		    }
-		},
-		less : {
 			dist : {
-				src : ['demo/style.less'],
-				dest : 'demo/style.css'
+				files : [
+					'src/jquery.loadprogress.js'
+				],
+				tasks:'concat min'
+			},
+			css : {
+				files : [
+					'src/jquery.loadprogress.css'
+				],
+				tasks:'concat'
 			}
 		},
 		concat:  {
 			dist : {
 				src : [
-					'src/jquery-ui-1.9.1.custom.js',
-					'src/jquery-parallax-plugin2.js'
+					'src/jquery.loadprogress.js'
 				],
-				dest : 'jquery-parallax-plugin2.js'
+				dest : 'jquery.loadprogress.js'
 			},
 			css : {
 				src : [
-					'src/jquery-ui-1.9.1.custom.css',
-					'src/jquery-parallax-plugin2.css'
+					'src/jquery.loadprogress.css'
 				],
-				dest : 'jquery-parallax-plugin2.css'
+				dest : 'jquery.loadprogress.css'
 			}
 		},
 		cssmin : {
 			dist : {
-				src : ['jquery-parallax-plugin2.css'],
-				dest : 'jquery-parallax-plugin2.min.css'
+				src : ['jquery.loadprogress.css'],
+				dest : 'jquery.loadprogress.min.css'
 			}
 		},
 		min: {
 			dist :{
-				src : ['jquery-parallax-plugin2.js'],
-				dest : 'jquery-parallax-plugin2.min.js'
+				src : ['jquery.loadprogress.js'],
+				dest : 'jquery.loadprogress.min.js'
 			}
 		}
 	});
-	  grunt.registerTask('default', 'concat min cssmin less');
+	  grunt.registerTask('default', 'concat min cssmin');
 };
--- jquery.loadprogress.js
+++ jquery.loadprogress.js
@@ -1,76 +1,79 @@
+ /*global $:true, jQuery:true */
  /*
  * jQuery Load Progress Plugin 1.1.1
  * Author : Otto Kamiya (MegazalRock)
  * License : Dual licensed under the MIT or GPL Version 2 licenses.
  * Browser : Chrome23+ (Win/Mac) Firefox14+ (Win/Mac) Opera12+ (Win/Mac) Safari6+(Mac) IE9+(Win) IE8(Win)
- * History : 1.1.1 Bug ifx
- * 			 1.1 Add Manual Mode 
- * 			 1.0.2 Bug Fix for IE
- * 			 1.0.1 Mini fix.
- * 			 1.0 Initial Release.
+ * History :
+ * 1.1.2    New Default Design and mini fix
+ * 1.1.1    Bug fix
+ * 1.1.0    Add Manual Mode
+ * 1.0.2    Bug Fix for IE
+ * 1.0.1    Mini fix.
+ * 1.0.0    Initial Release.
  */
 (function(){
-    $.extend({
-    	loadprogress:function(_options){
-    		_options = _options || {};
-    		var options = {
-    			imgSelector:'img',
-    			easing:'swing',
-    			duration:'1000',
-    			showText:true,
-    			manualMode:false,
-    			manualModeStopNum:95,
-    			bodyOverflowFixTo:false //false, auto ,hidden
-    		};
-    		$.extend(options,_options);
+	$.extend({
+		loadprogress:function(_options){
+			_options = _options || {};
+			var options = {
+				imgSelector:'img',
+				easing:'swing',
+				duration:'1000',
+				showText:true,
+				manualMode:false,
+				manualModeStopNum:95,
+				bodyOverflowFixTo:false //false, auto ,hidden
+			};
+			$.extend(options,_options);
 
 			var bodyElem = document.getElementsByTagName('body')[0];
 			if(options.bodyOverflowFixTo){
 				bodyElem.style.overflow = 'hidden';
 			}
-    		
-    		var $_overlay = $('<div id="loadprogress-overlay" />');
-    		$_overlay
-    			.css({
-    				height:$(window).height()
-    			});
-    		var $_box = $('<div id="loadprogress-box" />');
-    		var $_bar = $('<div id="loadprogress-bar" />');
-    		if(options.showText){
-        		var $_text = $('<div id="loadprogress-text" />');
-        		$_text.text('0%');
-    		}
-    		
-    		function onResize(){
-    			$_overlay
-    				.css({
-    					height:$(window).height()
-    				});
-    		}
-    		$(window).resize(onResize).resize();
-    		
-    		var $_img = $(options.imgSelector);
-    		var imgNum = $_img.length;
-    		var loadedNum = 0;
-    		$_overlay
-    			.appendTo($('body'))
-    			.append($_box.append($_bar).append($_text));
-    		$(window)
-    			.one('loadprogressEnd',function(){
-    				if(!options.manualMode){
-    					onLoadProgressEnd();
-    				}
-    			})
-    			.bind('loadprogressProgress',function(e,loadedNum){
-    				onLoadProgressProgress(loadedNum);
-    			});
-    		if(options.manualMode){
-    			$(window).one('loadprogressManualEnd',function(){
+			
+			var $_overlay = $('<div id="loadprogress-overlay" />');
+			$_overlay
+				.css({
+					height:$(window).height()
+				});
+			var $_box = $('<div id="loadprogress-box" />');
+			var $_bar = $('<div id="loadprogress-bar" />');
+			if(options.showText){
+				var $_text = $('<div id="loadprogress-text" />');
+				$_text.text('0%');
+			}
+			
+			function onResize(){
+				$_overlay
+					.css({
+						height:$(window).height()
+					});
+			}
+			$(window).resize(onResize).resize();
+			
+			var $_img = $(options.imgSelector);
+			var imgNum = $_img.length;
+			var loadedNum = 0;
+			$_overlay
+				.appendTo($('body'))
+				.append($_box.append($_bar).append($_text));
+			$(window)
+				.one('loadprogressEnd',function(){
+					if(!options.manualMode){
+						onLoadProgressEnd();
+					}
+				})
+				.bind('loadprogressProgress',function(e,loadedNum){
+					onLoadProgressProgress(loadedNum);
+				});
+			if(options.manualMode){
+				$(window).one('loadprogressManualEnd',function(){
 					onLoadProgressEnd();
-    			});
-    		}
-    		
-    		function onLoadProgressEnd(){
+				});
+			}
+			
+			function onLoadProgressEnd(){
 				$_bar
 					.stop(true,false)
 					.animate({
@@ -89,9 +92,9 @@
 						$(this).remove();
 						$(window).off('resize',onResize);
 					});
-    		}
-    		function onLoadProgressProgress(loadedNum){
-    			var per = (options.manualMode)?loadedNum / imgNum * 100 - (100 - options.manualModeStopNum):(loadedNum / imgNum * 100);
+			}
+			function onLoadProgressProgress(loadedNum){
+				var per = (options.manualMode)?loadedNum / imgNum * 100 - (100 - options.manualModeStopNum):(loadedNum / imgNum * 100);
 				$_bar
 					.css({
 						width:per + '%'
@@ -99,22 +102,22 @@
 				if(options.showText){
 					$_text
 						.text(Math.floor(per) + '%');
-				};
-    		}
-    		
-    		$_img.each(function(n){
+				}
+			}
+			
+			$_img.each(function(){
 				var src = $(this).attr('src');
 				$('<img />')
 					.one('load',function(){
-	    				loadedNum += 1;
-	    				if(imgNum === loadedNum){
-		    				$(window).trigger('loadprogressEnd');
-	    				}else{
-	    					$(window).trigger('loadprogressProgress',loadedNum);
-	    				}
-	    			})
-	    			.attr('src',src);
-    		});
-    	}
-    });
+						loadedNum += 1;
+						if(imgNum === loadedNum){
+							$(window).trigger('loadprogressEnd');
+						}else{
+							$(window).trigger('loadprogressProgress',loadedNum);
+						}
+					})
+					.attr('src',src);
+			});
+		}
+	});
 })(jQuery);
